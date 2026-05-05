fe296da6cf44904c917c71bce54528613ef13818
Tanhhh1 <anhtranduy25@gmail.com>
2026-05-05 17:14:34 +0700

    Set up project (Interface, Repositoriy, Uow)

---
+++ API-FlightSystem/Application/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Application.Interfaces.Repositories;
+
+namespace Application.Interfaces.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
---
+++ API-FlightSystem/Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,75 @@
+﻿using Application.Interfaces.Repositories;
+using Application.Interfaces.UnitOfWork;
+using Infrastructure.Persistences;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore.Storage;
+
+
+namespace Infrastructure.Uow
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _dbContext;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task BeginTransactionAsync()
+        {
+            if (_dbContext.Database.CurrentTransaction == null)
+            {
+                _transaction = await _dbContext.Database.BeginTransactionAsync();
+            }
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+                if (_transaction != null) await _transaction.CommitAsync();
+            }
+            catch
+            {
+                await RollbackTransactionAsync();
+                throw;
+            }
+            finally
+            {
+                await DisposeTransactionAsync();
+            }
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.RollbackAsync();
+                await DisposeTransactionAsync();
+            }
+        }
+
+        private async Task DisposeTransactionAsync()
+        {
+            if (_transaction != null)
+            {
+                await _transaction.DisposeAsync();
+                _transaction = null;
+            }
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _transaction?.Dispose();
+            _dbContext.Dispose();
+        }
+    }
+}
