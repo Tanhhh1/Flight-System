8c3959adfc1a680559a0b419293b2f2196770df9
Tanhhh1 <anhtranduy25@gmail.com>
2026-05-11 00:50:11 +0700

    Update migration

---
+++ API-FlightSystem/Infrastructure/Migrations/20260507061017_UpdateUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Airports_AirportCode",
+                table: "Airports",
+                column: "AirportCode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Airports_AirportCode",
+                table: "Airports");
+        }
+    }
+}
