a67e7d65455d7df35b0693e74d087ffb13611e82
Tanhhh1 <anhtranduy25@gmail.com>
2026-05-11 01:03:27 +0700

    Add mapping for project

---
+++ API-FlightSystem/Application/Mappers/MappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using Application.CQRS.Accounts.DTOs;
+using Application.CQRS.Routes.DTOs;
+using Domain.Entities;
+using Domain.Identity;
+using Mapster;
+
+namespace Application.Mappers
+{
+    public class MappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<Route, RouteDto>()
+                .Map(dest => dest.OriginAirportCode, src => src.OriginAirport.AirportCode)
+                .Map(dest => dest.DestinationAirportCode, src => src.DestinationAirport.AirportCode);
+
+            config.NewConfig<User, AccountDto>()
+                .Ignore(dest => dest.Roles);
+        }
+    }
+}
