15d2c2d7cda0ee903e681d37641c79f325ffb3ba
Tanhhh1 <anhtranduy25@gmail.com>
2026-05-14 00:47:55 +0700

    Update CQRS for review, account + Add CQRS, repository, controller for Auth

---
+++ API-FlightSystem/Application/Interfaces/Repositories/IRefreshTokenRepository.cs
@@ -0,0 +1,9 @@
+﻿using Application.Interfaces.Repositories.Common;
+using Domain.Identity;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IRefreshTokenRepository : IBaseRepository<RefreshToken>
+    {
+    }
+}
